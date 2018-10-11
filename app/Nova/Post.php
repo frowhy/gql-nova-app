594e261d4a4f27b0fad3b08ca4c1cf933c8fb41d
frowhy <frowhy@gmail.com>
2018-10-11 11:00:04 +0800

    :sparkles: Creating Nova Model By Post
    Signed-off-by: frowhy <frowhy@gmail.com>

---
+++ app/Nova/Post.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Nova;
+
+use CustomConfiguration\ResourceLabel;
+use Frowhy\NovaFieldQuill\NovaFieldQuill;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Panel;
+use Spatie\TagsField\Tags;
+
+class Post extends Resource
+{
+    use ResourceLabel;
+
+    public static $label = '帖子';
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\Post';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'title', 'seo_title', 'seo_description',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('标题', 'title'),
+
+            NovaFieldQuill::make('内容', 'content'),
+
+            new Panel('SEO 字段', $this->seoFields()),
+
+            BelongsTo::make('User')->searchable(),
+
+            DateTime::make('创建时间', 'created_at')->sortable()->onlyOnDetail(),
+
+            DateTime::make('更新时间', 'updated_at')->sortable()->exceptOnForms(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+
+    protected function seoFields()
+    {
+        return [
+            Text::make('SEO 标题', 'seo_title')->hideFromIndex(),
+
+            Tags::make('SEO 关键词')->type('seo_keywords')->hideFromIndex(),
+
+            Textarea::make('SEO 描述', 'seo_description')->hideFromIndex(),
+        ];
+    }
+}
